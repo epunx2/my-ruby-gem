c44197890486d91a40d74c15ce2cef43f95e9045
Eric Chrobak <echrobak@catalystconnect.com>
2022-02-02 22:10:04 -0600

    initializing repository

---
+++ file.rb
@@ -0,0 +1,31 @@
+class Filechange
+    attr_accessor :first_name, :last_name, :age
+
+    def initialize(params = {})
+        @first_name = params.fetch(:first_name)
+        @last_name = params.fetch(:last_name)
+        @age = params.fetch(:age)
+    end
+
+    def process()
+        "#{first_name} | #{last_name} | #{age} \r"
+    end
+
+    def write(content)
+        File.write("content.txt", content, mode: "a")
+    end
+end
+
+def add_to_file(file_content)
+    file_name = Filechange.new(file_content)
+    new_content = file_name.process()
+    file_name.write(new_content)
+end
+
+file_content = {:first_name => 'John', :last_name => 'Adams', :age => 85}
+add_to_file(file_content)
+
+# TODO:
+# 1. Create a receiving method
+# 2. Add iteration to the method
+# 3. Create a repository
